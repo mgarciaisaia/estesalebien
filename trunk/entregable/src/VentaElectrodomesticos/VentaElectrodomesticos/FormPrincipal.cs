31d83957fb6fc690b6d7d7c4844b7e5784766c60
desert69 <desert69@242996a8-fa60-3106-5b59-5fc131e8cc76>
2011-06-05 22:03:31 +0000

    Tratando de aplicar los cambios de fer sin copiar los binarios y cosas platform-dependent

--- trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/FormPrincipal.cs
+++ trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/FormPrincipal.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,17 +19,76 @@ using VentaElectrodomesticos.EfectuarPago;
 using VentaElectrodomesticos.TableroControl;
 using VentaElectrodomesticos.ClientesPremium;
 using VentaElectrodomesticos.MejoresCategorias;
+using VentaElectrodomesticos.MetodosSQL;
 
 namespace VentaElectrodomesticos
 {
+    
+    
     public partial class FormPrincipal : Form
     {
+        private ClaseSQL sql;
+        private string username;
+        
         public FormPrincipal()
         {
             InitializeComponent();
-
             FormLogin login = new FormLogin();
             login.ShowDialog(this);
+            username = login.USERNAME;
+            this.Text += " - " + username;
+            foreach (string s in login.FUNCIONES)
+            {
+                sql = new ClaseSQL();
+                sql.Open();
+                String defaultquery = " SELECT DISTINCT ROL_FUNCION FROM [MAYUSCULAS_SIN_ESPACIOS].ROLES ";
+                String query = " WHERE ROL_NOMBRE = '" + s + "'";
+                query = defaultquery + query;
+                SqlDataReader reader = sql.busquedaSQLDataReader(query);
+                while (reader.Read())
+                {
+                    //MessageBox.Show(reader[0].ToString());
+                    switch (reader[0].ToString())
+                    {
+                        case "ABM de Empleado":
+                            aBMToolStripMenuItem.Enabled = true;
+                            break;
+                        case "ABM de Rol":
+                            aBMDeRolToolStripMenuItem.Enabled = true;
+                            break;
+                        case "ABM de Usuario":
+                            aBMDeUsuarioToolStripMenuItem.Enabled = true;
+                            break;
+                        case "ABM de Cliente":
+                            aBMDeClienteToolStripMenuItem.Enabled=true;
+                            break;
+                        case "ABM de Producto":
+                            aBMDeProductoToolStripMenuItem.Enabled = true;
+                            break;
+                        case "Asignacion de stock":
+                            asignaciónDeStockToolStripMenuItem.Enabled=true;
+                            break;
+                        case "Facturacion":
+                            facturaciónToolStripMenuItem.Enabled = true;
+                            break;
+                        case "Efectuar Pago":
+                            efectuarPagoToolStripMenuItem.Enabled=true;
+                            break;
+                        case "Tablero de Control":
+                            tableroDeControlToolStripMenuItem.Enabled=true;
+                            break;
+                        case "Clientes Premium":
+                            clientesPremiumToolStripMenuItem.Enabled=true;
+                            break;
+                        case "Mejores Categorias":
+                            mejoresCategoriasToolStripMenuItem.Enabled = true;
+                            break;
+                    }
+                }
+                reader.Close();
+                sql.Close();
+                
+            }
         }
 
         private void aBMToolStripMenuItem_Click(object sender, EventArgs e)
@@ -95,6 +155,12 @@ namespace VentaElectrodomesticos
         {
             FormMejoresCategorias form = new FormMejoresCategorias();
             form.ShowDialog(this);
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            if (username == "")
+                this.Close();
         }
     }
 }
