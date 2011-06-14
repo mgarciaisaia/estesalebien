b82aa2139fc0dfa1fe66c14c196349d395e3a345
feramoroso@gmail.com <feramoroso@gmail.com@242996a8-fa60-3106-5b59-5fc131e8cc76>
2011-06-14 20:41:29 +0000

    Clase Empleado y cliente

---
+++ trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/AbmEmpleado/Empleado.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentaElectrodomesticos.AbmEmpleado
+{
+    public class Empleado
+    {
+        private string dni;
+        public string DNI
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string apellido;
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        private string mail;
+        public string Mail
+        {
+            get { return mail; }
+            set { mail = value; }
+        }
+
+        private string tefono;
+        public string Telefono
+        {
+            get { return tefono; }
+            set { tefono = value; }
+        }
+
+        private string direccion;
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private string provincia;
+        public string Provincia
+        {
+            get { return provincia; }
+            set { provincia = value; }
+        }
+
+        private string tipo;
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private string sucursal;
+        public string Sucursal
+        {
+            get { return sucursal; }
+            set { sucursal = value; }
+        }
+
+        private bool habilitado;
+        public bool Habilitado
+        {
+            get { return habilitado; }
+            set { habilitado = value; }
+        }
+
+    }
+}
