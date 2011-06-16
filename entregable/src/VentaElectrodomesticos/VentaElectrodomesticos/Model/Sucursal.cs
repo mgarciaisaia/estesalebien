6ee8c0a698e9cde4831da7567191fcf0c10155e0
Matias Garcia Isaia <mgarciaisaia@users.noreply.github.com>
2011-06-16 16:58:45 +0000

    Mas DAOs y eso

---
+++ entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/Model/Sucursal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentaElectrodomesticos.Model
+{
+    class Sucursal
+    {
+        public byte provincia { get; set; }
+        public byte tipo { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+
+        public Sucursal() { }
+        public Sucursal(byte provincia, byte tipo, String direccion, String telefono)
+        {
+            this.provincia = provincia;
+            this.tipo = tipo;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+    }
+}
