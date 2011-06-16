9d698a28ae06e248db987ef6ecab633fb8fd2739
desert69 <desert69@242996a8-fa60-3106-5b59-5fc131e8cc76>
2011-06-16 22:21:13 +0000

    Corregido el bug que me hizo el merge

--- trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/AsignacionStock/FormAsignacionStock.cs
+++ trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/AsignacionStock/FormAsignacionStock.cs
@@ -35,41 +35,6 @@ namespace VentaElectrodomesticos.AsignacionStock
         private void bProducto_Click(object sender, EventArgs e)
         {
             BuscadorProducto buscadorProductos = new BuscadorProducto();
-            buscadorProductos.ShowDialog(using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using VentaElectrodomesticos.Buscadores;
-using VentaElectrodomesticos.Model;
-using VentaElectrodomesticos.DAO;
-
-namespace VentaElectrodomesticos.AsignacionStock
-{
-    public partial class FormAsignacionStock : Form
-    {
-        private Empleado auditor = null;
-        private Producto producto = null;
-
-        public FormAsignacionStock()
-        {
-            InitializeComponent();
-        }
-
-        private void bAuditor_Click(object sender, EventArgs e)
-        {
-            BuscadorEmpleado buscadorEmpleados = new BuscadorEmpleado(false, "Analista");
-            buscadorEmpleados.ShowDialog(this);
-            auditor = buscadorEmpleados.getEmpleado();
-            tAuditor.Text = auditor.apellido.ToUpper() + ", " + auditor.nombre;
-        }
-
-        private void bProducto_Click(object sender, EventArgs e)
-        {
-            BuscadorProducto buscadorProductos = new BuscadorProducto();
             buscadorProductos.ShowDialog(this);
             producto = buscadorProductos.getProducto();
             tProducto.Text = producto.nombre;
@@ -139,62 +104,6 @@ namespace VentaElectrodomesticos.AsignacionStock
             groupStock.Enabled = false;
             Stocks.getInstance().nuevoMovimiento(producto, auditor, sucursales[tSucursalSeleccionada.Text], cNuevoStock.Value - stockActual);
             mostrarStock();
-        }
-
-    }
-}
-this);
-            producto = buscadorProductos.getProducto();
-            tProducto.Text = producto.nombre;
-        }
-
-        private void FormAsignacionStock_Load(object sender, EventArgs e)
-        {
-            cSucursal.Items.Clear();
-            cSucursal.Items.Add("");
-            foreach (Sucursal sucursal in Sucursales.getInstance().list())
-            {
-                cSucursal.Items.Add(Provincias.getInstance().provincia(sucursal.provincia).nombre);
-            }
-        }
-
-        private void bBuscarProducto_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                validarProducto();
-                mostrarStock();
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("No se pudo buscar el stock del producto seleccionado.\n\n" + exception.Message);
-            }
-        }
-
-        private void validarProducto()
-        {
-            String errores = "";
-            if (tProducto.Text.Length == 0)
-            {
-                errores += "\nDebe seleccionar un producto";
-            }
-            if (tAuditor.Text.Length == 0)
-            {
-                errores += "\nDebe seleccionar un auditor";
-            }
-            if (cSucursal.Text.Length == 0)
-            {
-                errores += "\nDebe seleccionar una sucursal";
-            }
-            if (errores.Length > 0)
-            {
-                throw new Exception("Faltan campos:" + errores);
-            }
-        }
-
-        private void mostrarProducto()
-        {
-
         }
 
     }
