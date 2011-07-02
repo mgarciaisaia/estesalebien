a5e43b3db6ce69b42ab1c0b67c965e010cd3ca84
desert69 <desert69@242996a8-fa60-3106-5b59-5fc131e8cc76>
2011-07-02 01:51:28 +0000

    ABMRoles: Funcionalidades
    Cambiado el array de Funcionalidades por un DAO cacheado

---
+++ trunk/entregable/src/VentaElectrodomesticos/VentaElectrodomesticos/Model/Funcionalidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentaElectrodomesticos.DAO
+{
+    class Funcionalidad
+    {
+        public byte codigo { get; set; }
+        public String descripcion {get;set;}
+        public Funcionalidad() { }
+        public Funcionalidad(byte codigo, String descripcion)
+        {
+            this.codigo = codigo;
+            this.descripcion = descripcion;
+        }
+    }
+}
